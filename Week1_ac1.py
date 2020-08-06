8c733aaf6167661a350bd1bedb9ab6968f246972
mdbandi123 <69296355+mdbandi123@users.noreply.github.com>
2020-08-06 15:14:16 +0800

    Add files via upload

---
+++ Week1_ac1.py
@@ -0,0 +1,89 @@
+lista = ["sun", "moon", "star", "cloud"]
+lista2 =["King", "King", "Queen"]
+s1 = [1,2,3,4,5,1,2,3,1,1,1,3]
+
+##print(lista)
+##print(s1)
+##print(lista[2])
+##print(lista[1:3])
+##print(lista[1:])
+
+
+#FROM THE END
+##print(lista[-2])
+##print(lista[:-2])
+##print(lista[-2:])
+
+#FOR CHANGING VALUES
+##print(lista)
+##lista[1] = "Sumang"
+##print(lista)
+##
+
+#FOR REPEATING
+##for i in lista:
+##    print(i)
+##for i in lista:
+##    print(i, end="-")
+
+#FOR CONDITIONAL
+##if "King" in lista:
+##    print("King is in the list")
+##else:
+##    print("King is not in the list")
+
+##search = input("Enter a string to search")
+##if search in lista:
+##    print(search,"is in the list")
+##else:
+##    print(search,"is not in the list")
+
+#FOR ADDING(specific index)/APPENDING(at the end)/EXTENDING
+##print("Before:",lista)
+##x = input("Enter variable:")
+##y = int(input("Enter index:"))
+##lista.insert(y,x)
+##print("After:",lista)
+##lista.extend(s1)
+##print(lista)
+##lista.append(s1)
+##print(lista)
+##print("Before:",lista)
+##x = input("Enter variable:")
+##lista.append(x)
+##print("After:",lista)
+
+#FOR REMOVING(specific variable/POPPING(last item/specific index)
+##print("Before:",lista)
+##x = input("Enter variable:")
+##lista.remove(x)
+##print("After:",lista)
+
+##print("Before:",lista)
+##x = int(input("Enter index:"))
+##lista.pop(x)
+##print("After:",lista)
+
+#FOR COUNTING SPECIFIC NUMBER OF TIMES A VARIABLE APPEARS)
+##print("No of 1 is ",s1.count(1))
+##print("No of 2 is ",s1.count(2))
+##print("No of 3 is ",s1.count(3))
+##print("No of King is",lista2.count("King"))
+
+#FOR SORTING AND REVERSING
+##print("Original State:",lista)
+##lista.sort()
+##print(lista)
+##lista.reverse()
+##print(lista)
+
+#FOR COPYING
+##print("Original:",lista)
+##copy = lista.copy()
+##print(copy)
+
+##print("Original:",lista)
+##print("Original:",s1)
+##s1 = lista.copy()
+##print("After Copying lista to s1:",s1)
+
